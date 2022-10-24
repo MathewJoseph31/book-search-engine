f7127b4df46784c3620f6e2af9c69be9b42a62bc
mathew joseph <mathew_joseph31@yahoo.com>
2022-10-24 00:54:02 -0400

    unwanted files deleted (routes/controller files)

--- server/server.js
+++ server/server.js
@@ -10,15 +10,11 @@ const { authMiddleware } = require("./utils/auth");
 const path = require("path");
 const db = require("./config/connection");
 
-const routes = require("./routes");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//app.use(routes);
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers, app) => {
